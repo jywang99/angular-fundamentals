9ccd247629b276f8200a534e79c2a062d4ed255d
jywang99 <junyi.wang.007@gmail.com>
2022-06-10 05:44:38 +0900

    using third-party library as service

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { ToastrService } from "../common/toastr.service";
 import { EventService } from "./shared/event.service";
 
 @Component({
@@ -9,7 +10,7 @@ import { EventService } from "./shared/event.service";
         <hr />
         <div class="row">
             <div *ngFor="let event of events" class="col-md-5">
-            <event-thumbnail [event]="event"></event-thumbnail>
+                <event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></event-thumbnail>
             </div>
         </div>
     </div>
@@ -18,11 +19,15 @@ import { EventService } from "./shared/event.service";
 export class EventsListComponent implements OnInit {
     events: any[] | undefined;
 
-    constructor(private eventService: EventService) {
+    constructor(private eventService: EventService, private toastr: ToastrService) {
         
     }
 
     ngOnInit() {
         this.events = this.eventService.getEvents();
+    }
+
+    handleThumbnailClick(eventName: string) {
+        this.toastr.success(eventName);
     }
 }
