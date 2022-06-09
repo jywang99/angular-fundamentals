90aa6f8a779525dd4d36bd2b67318b8cbb91fd63
jywang99 <junyi.wang.007@gmail.com>
2022-06-10 03:11:41 +0900

    cleanup

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -18,9 +18,4 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
 })
 export class EventThumbnailComponent {
     @Input() event: any
-    someProperty:any = "some value"
-    
-    logFoo() {
-        console.log('foo');
-    }
 }
