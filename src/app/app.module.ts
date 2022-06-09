21495ffcba264c0bc8e636dff2f5eded33a4e82b
jywang99 <junyi.wang.007@gmail.com>
2022-06-10 02:40:14 +0900

    create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
 
 @NgModule({
   declarations: [ // components
-    EventsAppComponent
+    EventsAppComponent,
+    EventsListComponent
   ],
   imports: [ // other modules
     BrowserModule
