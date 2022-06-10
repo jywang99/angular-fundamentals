6ee316df7b270c1a0ca2b94f94c987cc0facd2c3
jywang99 <junyi.wang.007@gmail.com>
2022-06-11 04:10:38 +0900

    guarding against route activation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ToastrService } from './common/toastr.service';
+import { Error404Component } from './errors/404.component';
 
 import { EventsAppComponent } from './events-app.component';
 import { CreateEventComponent } from './events/create-events.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventService } from './events/shared/event.service';
@@ -19,7 +21,8 @@ import { appRoutes } from './routes';
     EventThumbnailComponent,
     NavBarComponent,
     EventDetailsComponent,
-    CreateEventComponent
+    CreateEventComponent,
+    Error404Component
   ],
   imports: [ // other modules
     BrowserModule,
@@ -27,7 +30,8 @@ import { appRoutes } from './routes';
   ],
   providers: [ // services
     EventService,
-    ToastrService
+    ToastrService,
+    EventRouteActivator
   ],
   bootstrap: [EventsAppComponent]
 })
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,18 +1,23 @@
 import { Routes } from "@angular/router";
+import { Error404Component } from "./errors/404.component";
 import { CreateEventComponent } from "./events/create-events.component";
 import { EventDetailsComponent } from "./events/event-details/event-details.component";
+import { EventRouteActivator } from "./events/event-details/event-route-activator.service";
 import { EventsListComponent } from "./events/events-list.component";
 
 
 export const appRoutes: Routes = [
     // new event page
     { path: 'events/new', component: CreateEventComponent },
-    
+
     // main page listing events
     { path: 'events', component: EventsListComponent },
 
     // id: URI parameter
-    { path: 'events/:id', component: EventDetailsComponent },
+    { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
+
+    // not found
+    {path: '404', component: Error404Component},
 
     // redirects to /events if path is empty (default)
     // pathMatch: full=exact match, prefix=prefix match
