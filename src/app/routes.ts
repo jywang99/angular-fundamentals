f5b6446aa74d1fdfcc3d39bdd1372ab5ce49e67d
jywang99 <junyi.wang.007@gmail.com>
2022-06-11 03:56:23 +0900

    routing from ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from "@angular/router";
+import { CreateEventComponent } from "./events/create-events.component";
 import { EventDetailsComponent } from "./events/event-details/event-details.component";
 import { EventsListComponent } from "./events/events-list.component";
 
 
 export const appRoutes: Routes = [
+    // new event page
+    { path: 'events/new', component: CreateEventComponent },
+    
+    // main page listing events
     { path: 'events', component: EventsListComponent },
 
     // id: URI parameter
