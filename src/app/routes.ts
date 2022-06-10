09128eb145102d6e73b8f6dbb71fb2a6fc257862
jywang99 <junyi.wang.007@gmail.com>
2022-06-11 05:11:34 +0900

    add user module

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -9,18 +9,24 @@ import { EventsListComponent } from "./events/events-list.component";
 
 export const appRoutes: Routes = [
     // new event page
-    { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent']},
+    { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
 
     // main page listing events
-    { path: 'events', component: EventsListComponent, resolve: {events: EventsListResolver} },
+    { path: 'events', component: EventsListComponent, resolve: { events: EventsListResolver } },
 
     // id: URI parameter
     { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
 
     // not found
-    {path: '404', component: Error404Component},
+    { path: '404', component: Error404Component },
 
     // redirects to /events if path is empty (default)
     // pathMatch: full=exact match, prefix=prefix match
     { path: '', redirectTo: '/events', pathMatch: 'full' },
+
+    { // when path starts from user, load module UserModule from the file path specified
+        path: 'user',
+        loadChildren: () => import('./user/user.module')
+            .then(m => m.UserModule)
+    }
 ]
---
+++ src/app/user/user.routes.ts
@@ -0,0 +1,6 @@
+import { ProfileComponent } from "./profile.component";
+
+export const userRoutes = [
+    // full path: /user/profile
+    { path: 'profile', component: ProfileComponent }
+]
